31bb7729f003c3105e95b24d05fa0e628640b596
Sarah Charity <55107755+char1129@users.noreply.github.com>
2020-03-04 03:55:30 -0500

    Add in a plus and forward slash n
    to satisfy markbot

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@ var creature = prompt('A creature (dragon, moth, etc.)');
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verbed = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning Enter the letter below:');
+
 alert('Let’s make a story together!');
 
 switch (story) {
   case 'a' :
-        document.write(`After snacking on ${adjective} treats, the ${colour} bellied ${creature} ${verbed} for hours.`);
+    document.write(`After snacking on ${adjective} treats, the ${colour} bellied ${creature} ${verbed} for hours.`);
     break;
 
   case 'b' :
-    document.write(`Amelia ${verbed} through the ${adjective} ${colour} nebula escaping the space ${creature}.`);
+    document.write(`Amelia ${verbed} through the ${adjective} ${colour} nebula escaping the space ${creature}+.`);
     break;
 
   case 'c' :
     document.write(`Jackson chose his ${adjective}, ${colour} ${creature} card and ${verbed} it to the table knowing he won.`);
     break;
 };
+
+// \n
